f5de2e847cc2a367c203a8e923300df4a38cea72
helenakgg <122775236+helenakgg@users.noreply.github.com>
2023-07-20 00:49:27 +0700

    update models

--- src/models/all.models.js
+++ src/models/all.models.js
@@ -43,3 +43,91 @@ export const User = db.sequelize.define("users", {
 },
 )
 
+export const Category = db.sequelize.define("categories", {
+    id: {
+        type: db.Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    name: {
+        type : db.Sequelize.STRING(45),
+        allowNull : false
+    }
+},
+{ timestamps: false }
+);
+
+export const Article = db.sequelize.define("articles", {
+    id: {
+        type: db.Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    title: {
+        type : db.Sequelize.STRING(45),
+        allowNull : false
+    },
+    imageURL : {
+        type : db.Sequelize.STRING(255),
+        allowNull : true,
+    },
+    content : {
+        type : db.Sequelize.TEXT('long'),
+        allowNull : false
+    },
+    videoURL : {
+        type : db.Sequelize.STRING(255),
+        allowNull : true,
+    },
+    country : {
+        type : db.Sequelize.STRING(45),
+        allowNull : true,
+    },
+    keywords : {
+        type : db.Sequelize.STRING(45),
+        allowNull : false,
+    },
+    userId: {
+        type: db.Sequelize.INTEGER,
+        allowNull : false
+    },
+    categoryId : {
+        type : db.Sequelize.INTEGER,
+        allowNull : false,
+    },  
+}
+)
+
+export const Like = db.sequelize.define("likes", {
+    id: {
+        type: db.Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    article_id: {
+        type: db.Sequelize.INTEGER,
+        allowNull : false
+    },
+    user_id: {
+        type: db.Sequelize.INTEGER,
+        allowNull : false
+    },
+    
+},
+)
+
+User.hasMany(Like);
+User.hasMany(Article);
+
+Category.hasMany(Article);
+
+Article.belongsTo(Category, {foreignKey : 'categoryId'});
+Article.belongsTo(User, {foreignKey : 'userId'});
+Article.hasMany(Like);
+
+Like.belongsTo(User, {foreignKey : 'user_id'});
+Like.belongsTo(Article, {foreignKey : 'article_id'});
+
